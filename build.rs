fa5f80a1e44f5f04f82cf57fdf754b865b06bad0
Marek Miller <mlm@math.ku.dk>
2023-06-12 12:13:28 +0200

    Add mpi gpu features

--- build.rs
+++ build.rs
@@ -1,12 +1,29 @@
 extern crate cmake;
 
 fn main() {
-    let dst = cmake::Config::new("QuEST")
-        .no_build_target(true)
+    let mut config = cmake::Config::new("QuEST");
+    let mut config = config.no_build_target(true);
+
+    config = config
         .define("MULTITHREADED", "ON")
         .define("GPUACCELERATED", "OFF")
-        .define("DISTRIBUTED", "OFF")
-        .build();
+        .define("DISTRIBUTED", "OFF");
+
+    if cfg!(feature = "gpu") {
+        config = config
+            .define("MULTITHREADED", "OFF")
+            .define("GPUACCELERATED", "ON")
+            .define("DISTRIBUTED", "OFF");
+    }
+
+    if cfg!(feature = "mpi") {
+        config = config
+            .define("MULTITHREADED", "ON")
+            .define("GPUACCELERATED", "OFF")
+            .define("DISTRIBUTED", "ON");
+    }
+
+    let dst = config.build();
 
     println!(
         "cargo:rustc-link-search=native={}/build/QuEST",
