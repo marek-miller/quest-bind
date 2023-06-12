b9a77ab8d6b84194ce5bab043d26e3225a082cd7
Marek Miller <mlm@math.ku.dk>
2023-06-12 11:05:37 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 extern crate cmake;
 
 fn main() {
-    let dst = cmake::Config::new("QuEST").no_build_target(true)
-    // .define("DISTRIBUTED", "ON")
-    .build();
+    let dst = cmake::Config::new("QuEST")
+        .no_build_target(true)
+        .define("MULTITHREADED", "ON")
+        .define("GPUACCELERATED", "OFF")
+        .define("DISTRIBUTED", "OFF")
+        .build();
+
     println!(
         "cargo:rustc-link-search=native={}/build/QuEST",
         dst.display()
