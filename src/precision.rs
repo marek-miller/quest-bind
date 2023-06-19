7e29cb0d1eec09f4b7df5d7d78725ffd39b74ac9
Marek Miller <mlm@math.ku.dk>
2023-06-19 15:11:53 +0200

    Use EPSILON defined on floating-point type
    instead of std::f64::consts value

--- src/precision.rs
+++ src/precision.rs
@@ -3,16 +3,15 @@ mod _precision {
     #[allow(non_camel_case_types)]
     pub type qreal = std::ffi::c_double;
     pub type Qreal = f64;
-    pub use std::f64::{
-        consts::{
-            LN_10,
-            LN_2,
-            PI,
-            SQRT_2,
-            TAU,
-        },
-        EPSILON,
+    pub use std::f64::consts::{
+        LN_10,
+        LN_2,
+        PI,
+        SQRT_2,
+        TAU,
     };
+    /// Machine epsilon value for [`Qreal`](crate::Qreal)
+    pub const EPSILON: Qreal = f64::EPSILON;
 }
 
 #[cfg(feature = "f32")]
@@ -20,16 +19,15 @@ mod _precision {
     #[allow(non_camel_case_types)]
     pub type qreal = std::ffi::c_float;
     pub type Qreal = f32;
-    pub use std::f32::{
-        consts::{
-            LN_10,
-            LN_2,
-            PI,
-            SQRT_2,
-            TAU,
-        },
-        EPSILON,
+    pub use std::f32::consts::{
+        LN_10,
+        LN_2,
+        PI,
+        SQRT_2,
+        TAU,
     };
+    /// Machine epsilon value for [`Qreal`](crate::Qreal)
+    pub const EPSILON: Qreal = f32::EPSILON;
 }
 
 pub use _precision::{
