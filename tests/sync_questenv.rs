f6445d1314d80832d5c09b155b0043cf2678937a
Marek Miller <mlm@math.ku.dk>
2023-07-12 15:25:38 +0200

    Add tests

--- tests/sync_questenv.rs
+++ tests/sync_questenv.rs
@@ -2,13 +2,18 @@ use std::thread;
 
 use quest_bind::{
     get_quest_seeds,
+    get_real_amp,
+    init_zero_state,
+    pauli_x,
     report_quest_env,
     seed_quest,
     seed_quest_default,
     QuestEnv,
+    Qureg,
+    EPSILON,
 };
 
-// This won't compile if QuestEnv is not Sync
+// This won't compile unless QuestEnv is Sync
 #[test]
 fn questenv_is_sync() {
     let env = QuestEnv::new();
@@ -64,4 +69,59 @@ fn questenv_seed_quest_default_01() {
         s.spawn(|| seeds_check = get_quest_seeds(&env));
     });
     assert_eq!(seeds, seeds_check);
+}
+
+fn compute_qureg(env: &QuestEnv) -> bool {
+    let qureg = &mut Qureg::try_new(2, env).unwrap();
+    init_zero_state(qureg);
+    pauli_x(qureg, 0).unwrap();
+    let amp = get_real_amp(qureg, 1).unwrap();
+
+    (amp - 1.).abs() < EPSILON
+}
+
+#[test]
+fn independent_qubit_ops_01() {
+    let env = QuestEnv::new();
+
+    assert!(compute_qureg(&env));
+
+    thread::scope(|s| {
+        s.spawn(|| assert!(compute_qureg(&env)));
+    });
+}
+
+#[test]
+fn independent_qubit_ops_02() {
+    let env = QuestEnv::new();
+
+    thread::scope(|s| {
+        s.spawn(|| assert!(compute_qureg(&env)));
+        s.spawn(|| assert!(compute_qureg(&env)));
+    });
+
+    assert!(compute_qureg(&env));
+}
+
+#[test]
+fn independent_qubit_ops_03() {
+    const NUM_REPS: usize = 100;
+
+    let env = QuestEnv::new();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            for _ in 0..NUM_REPS {
+                assert!(compute_qureg(&env))
+            }
+        });
+
+        s.spawn(|| {
+            for _ in 0..NUM_REPS {
+                assert!(compute_qureg(&env))
+            }
+        });
+    });
+
+    assert!(compute_qureg(&env));
 }
